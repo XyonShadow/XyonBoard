fd35817878e420b267cd32ac219846439a442af6
Michael <prncemk11@gmail.com>
2025-10-25 18:35:52 +0100

    style: add custom fade-in animation and keyframes to Tailwin and  simplify custom underline style in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ export default {
         3000: '3000ms'
       },
       keyframes: {
+        fadeIn: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
         fadeInUp: {
           '0%': { opacity: '0', transform: 'translateY(40px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
@@ -42,6 +46,7 @@ export default {
         },
       },
       animation: {
+        fadeIn: 'fadeIn 0.5s ease-out forwards',
         fadeInUp: 'fadeInUp 0.6s ease-out forwards',
         fadeInDown: 'fadeInDown 0.6s ease-out forwards',
         fadeInLeft: 'fadeInLeft 0.6s ease-out forwards',
